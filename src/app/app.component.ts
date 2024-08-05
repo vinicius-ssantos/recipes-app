85551aac0f3ed7030307b168bcb83aa67cb9acd6
Vinicius <viniciusolicveira@gmail.com>
2024-08-05 00:30:49 -0300

    iniciando componentes (footer,home-page,card)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {NavbarComponent} from "./pages/navbar/navbar.component";
+import {FooterComponent} from "./pages/footer/footer.component";
+import {HomePageComponent} from "./pages/home-page/home-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    NavbarComponent,
+    FooterComponent,
+    HomePageComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/pages/recipe-card/recipe-card.component.ts
@@ -0,0 +1,16 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
+
+@Component({
+  selector: 'app-recipe-card',
+  standalone: true,
+  imports: [MatCardModule, MatButtonModule],
+  templateUrl: './recipe-card.component.html',
+  styleUrl: './recipe-card.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RecipeCardComponent {
+
+}
